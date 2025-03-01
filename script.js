d029148c22e73b1ac07c1b46aaf92f256497bbeb
rszh11 <rs.ambi@icloud.com>
2025-03-01 23:24:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     alert('Vielen Dank für Ihre Nachricht!');
     this.reset(); // Formular zurücksetzen
 });
+function initMap() {
+    // Der Standort von Uluru
+    const position = { lat: -25.344, lng: 131.031 };
+
+    // Die Karte, zentriert auf Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: position,
+    });
+
+    // Die Markierung, positioniert auf Uluru
+    const marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: "Uluru",
+    });
+}
