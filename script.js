15e09bbbdb901d316aa60428df61a7036b6f5850
rszh11 <rs.ambi@icloud.com>
2025-03-17 20:44:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const originalHeight = footer.offsetHeight; // Ursprüngliche Höhe des Footers
 
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY; // Aktuelle Scroll-Position
-    const newHeight = Math.max(originalHeight - scrollPosition, 0); // Berechnung der neuen Höhe
+    const newHeight = Math.max(originalHeight - scrollPosition / 2, 50); // Berechnung der neuen Höhe (max. 50px)
     footer.style.height = newHeight + 'px'; // Setzen der neuen Höhe
-    footer.style.padding = newHeight > 0 ? '20px 0' : '0'; // Anpassen des Paddings
+    footer.style.padding = newHeight > 50 ? '20px 0' : '10px 0'; // Anpassen des Paddings
 });
